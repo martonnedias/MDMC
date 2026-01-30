ef0f3755d4eadee8c84899bce9ec0772beaf565a
martonnedias <martonnedias@gmail.com>
2026-01-29 22:44:41 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ base: '/MDMC/',
-  plugins: [react()] }) => {
+export default defineConfig(({ base: '/MDMC/',}) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
